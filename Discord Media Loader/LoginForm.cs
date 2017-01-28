29c9381f3ac411459a86dcde57bc2fbd77761c86
Serraniel <online-daniel95@web.de>
2017-01-28 17:17:19 +0100

    Added Login Form

---
+++ Discord Media Loader/LoginForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Discord_Media_Loader
+{
+    public partial class LoginForm : Form
+    {
+        public static bool Exec(ref string email, out string password)
+        {
+            var loginForm = new LoginForm { tbxEmail = { Text = email } };
+            password = "";
+
+            if (loginForm.ShowDialog() == DialogResult.OK)
+            {
+                email = loginForm.tbxEmail.Text;
+                password = loginForm.tbxPassword.Text;
+                return true;
+            }
+
+            return false;
+        }
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Abort;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
