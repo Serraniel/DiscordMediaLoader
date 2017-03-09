1ac96b3966c07b1bbb8abf427989b726cc76a77d
Serraniel <online-daniel95@web.de>
2017-03-09 20:08:35 +0100

    Recreated project files

--- Discord Media Loader/Program.cs
+++ Discord Media Loader/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-using Discord_Media_Loader.Helper;
 
 namespace Discord_Media_Loader
 {
@@ -11,10 +13,10 @@ namespace Discord_Media_Loader
         /// </summary>
         [STAThread]
         static void Main()
-        {            
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            //Application.Run(new Form1());
         }
     }
 }
