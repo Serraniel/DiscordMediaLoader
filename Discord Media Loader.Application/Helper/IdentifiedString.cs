801a0da615d9c2ade83aaea8497e7d29796c2605
Serraniel <serraniel@posteo.de>
2018-06-15 18:32:18 +0200

    Added class for identified strings

---
+++ Discord Media Loader.Application/Helper/IdentifiedString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DML.Application.Helper
+{
+    internal class IdentifiedString<T>
+    {
+        internal T Id { get; set; }
+        internal string Caption { get; set; }
+        
+        internal IdentifiedString(T id, string caption)
+        {
+            Id = id;
+            Caption = caption;
+        }
+
+        public override string ToString() => Caption;
+    }
+}
