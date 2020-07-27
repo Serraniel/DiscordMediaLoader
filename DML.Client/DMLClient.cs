80cab1ea445c66d18a269729d46c6f32c279066f
Serraniel <mail@serraniel.dev>
2020-07-27 11:26:01 +0200

    #34 Temporary disabled login code

--- DML.Client/DMLClient.cs
+++ DML.Client/DMLClient.cs
@@ -6,11 +6,12 @@ namespace DML.Client
 {
     public static class DMLClient
     {
-        public static DiscordSocketClient Client { get; set; } = new DiscordSocketClient(new DiscordSocketConfig(){DefaultRetryMode = RetryMode.RetryRatelimit|RetryMode.RetryTimeouts});
+        public static DiscordSocketClient Client { get; set; } = new DiscordSocketClient(new DiscordSocketConfig() { DefaultRetryMode = RetryMode.RetryRatelimit | RetryMode.RetryTimeouts });
 
         public static async Task<bool> Login(string token)
         {
-            await Client.LoginAsync(TokenType.User, token);
+            // TODO: Fix Login with usertoken
+            //await Client.LoginAsync(TokenType.User, token);
             await Client.StartAsync();
             await Task.Delay(1000);
 
